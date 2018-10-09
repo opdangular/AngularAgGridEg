b166dfafdf9427f3e9ea44b87dae817a4b26a3bc
Osmand Das <osmandpdas@gmail.com>
2018-10-08 21:27:22 -0400

    initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridNg2 } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild('agGrid') agGrid: AgGridNg2;
+
   title = 'app';
+  rowData: any;  
+
+  columnDefs = [
+    { headerName: 'Make', field: 'make', checkboxSelection: true},
+    { headerName: 'Model', field: 'model'},
+    { headerName: 'Price', field: 'price'}
+  ];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  /*
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000},
+    { make: 'Ford', model: 'Mondeo', price: 32000},
+    { make: 'Porsche', model: 'Boxter', price: 72000},
+  ];
+  */
+
+  ngOnInit(){
+    this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+  }
+
+  getSelectedRows(){
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map( node => node.data );
+    const selectedDataStringPresentation = selectedData.map( node => node.make + ' ' + node.model).join(', ');
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http'
+import 'ag-grid-enterprise'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AgGridModule.withComponents([]), HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
